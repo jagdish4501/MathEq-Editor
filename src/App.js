666911d05273aee30df682842f2e932b05216df4
jagdish <pkjagdish4501@gmail.com>
2024-06-16 14:08:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import '//unpkg.com/mathlive'
+import { useState, useRef, useEffect } from 'react'
+
+// Use &#123; and &#125; to escape { and } in JSX
 
 function App() {
+  const [value, setValue] = useState('')
+  const[show,setShow]=useState(false)
+  const [msg,setMsg]=useState('');
+
+  // Customize the mathfield when it is mounted
+  const mf = useRef()
+  useEffect(() => {
+    mf.current.smartFence = true
+    mf.current.addEventListener('input', (evt) => {
+      if (evt.inputType === 'insertLineBreak') {
+        evt.target.executeCommand('plonk')
+      }
+    })
+  }, [])
+
+  // Update the mathfield when the value changes
+  useEffect(() => {
+    mf.current.value = value
+  }, [value])
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1 style={{color:'white'}}>MathEQ Editor</h1>
+        <math-field ref={mf} onInput={(evt) => setValue(evt.target.value)} >
+          {value}
+        </math-field>
+      {value!==''?<>
+      <div className='LateX'> 
+        {value}
+      </div>
+        <button className='bn30' onClick={async()=>{
+          setShow(true);
+          setTimeout(()=>{setShow(false)},2000)
+          try{
+            await navigator.clipboard.writeText(value);
+            setMsg('copied to clipboard')
+            }catch(err){
+              setMsg('unable to copy')
+            }
+          }
+          }>copy LateX</button>
+      </>
+      :<></>
+      }
+      {
+        show?
+        <p style={{color:'white'}}>
+          {msg}
+        </p>:<></>
+      }
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
